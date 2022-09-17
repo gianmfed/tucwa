c7bc57b49e436f50800e62c338ea3a14b8d5ba4e
Gianmarco <gianmfed@gmail.com>
2022-09-17 17:14:30 +0200

    pyqt funziona

--- pyqt5testvenv/testpyqt.py
+++ pyqt5testvenv/testpyqt.py
@@ -6,8 +6,12 @@ import sys
 def window():
     app = QApplication(sys.argv)
     win = QMainWindow()
-    win.setGeometry(200, 200, 300, 300)
+    win.setGeometry(200, 200, 600, 300)
     win.setWindowTitle("TUCWA")
+
+    label = QtWidgets.QLabel(win)
+    label.setText("Label")
+    label.move(50, 50)
     
     win.show()
     sys.exit(app.exec_())
