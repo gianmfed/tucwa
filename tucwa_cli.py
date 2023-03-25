4514b82606ef5174ce0b4e725f5c519bd6aafd4b
gianmfed <gianmfed@gmail.com>
2023-03-25 16:37:18 +0100

    aggiungi commenti in tucwa_cli.py

--- tucwa_cli.py
+++ tucwa_cli.py
@@ -3,7 +3,12 @@
 
 from sqlite3 import connect
 
-
+# Function to convert seconds to HH:MM
+def sec_to_hhmm(seconds):
+    minutes = seconds // 60
+    hours = minutes // 60
+    minutes = minutes - hours * 60
+    return f"{hours}:{minutes}"
 
 # Connect time.db database
 con = connect('time.db')
@@ -12,15 +17,15 @@ db = con.cursor()
 last_seven_day = db.execute('SELECT * FROM time_table ORDER BY date DESC LIMIT 7')
 listadate = last_seven_day.fetchall()
 
-print(f'\nOggi: {listadate[0][1] // 60} minuti\n')
+print(f'\nOggi: {sec_to_hhmm(listadate[0][1])}\n')
 
 print('Ultima settimana:')
 sum_average = 0
 for data in listadate:
     sum_average += data[1] // 60
-    print(f'Giorno: {data[0]}, {data[1] // 60} minuti')
+    print(f'Giorno: {data[0]}, {sec_to_hhmm(data[1])} o {data[1]} minuti')
 
-print(f'\nMedia: {sum_average // 7} minuti al giorno\n')
+print(f'\nMedia: {sec_to_hhmm((sum_average * 60) // 7)} o {sum_average // 7} minuti al giorno\n')
 input()
 
 con.close()
